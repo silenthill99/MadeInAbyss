6750ab2c90ad94e7508f4a445f8d00ffe8c0fbf0
silenthill99 <florian.graziani@sfr.fr>
2024-04-28 16:04:10 +0200

    Rajout de la fonction pour créer les blocs

--- src/main/java/fr/silenthill99/madeinabyss/init/ModBlocks.java
+++ src/main/java/fr/silenthill99/madeinabyss/init/ModBlocks.java
@@ -1,10 +1,21 @@
 package fr.silenthill99.madeinabyss.init;
 
 import fr.silenthill99.madeinabyss.Main;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
 
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Main.MODID);
+
+    private static <T extends Block> RegistryObject<T> createBlock(String name, Supplier<T> supplier) {
+        RegistryObject<T> block = BLOCKS.register(name, supplier);
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+        return block;
+    }
 }
