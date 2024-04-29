07ab5c5d27ad98388009efb00750b8a367b0838c
BuildTools <unconfigured@null.spigotmc.org>
2024-04-29 12:03:46 +0200

    Rajout des tags

--- src/main/java/fr/silenthill99/madeinabyss/data/DataGeneration.java
+++ src/main/java/fr/silenthill99/madeinabyss/data/DataGeneration.java
@@ -4,6 +4,7 @@ import fr.silenthill99.madeinabyss.Main;
 import fr.silenthill99.madeinabyss.data.loot_tables.LootTableGenerator;
 import fr.silenthill99.madeinabyss.data.models_and_blockstates.BlockStateGenerator;
 import fr.silenthill99.madeinabyss.data.models_and_blockstates.ItemModelGenerator;
+import fr.silenthill99.madeinabyss.data.tags.BlockTagGenerator;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
@@ -29,6 +30,7 @@ public class DataGeneration {
         generator.addProvider(event.includeClient(), new ItemModelGenerator(packOutput, existingFileHelper));
 
         generator.addProvider(event.includeServer(), new LootTableGenerator(packOutput));
+        generator.addProvider(event.includeServer(), new BlockTagGenerator(packOutput, lookupProvider, existingFileHelper));
 
     }
 
---
+++ src/main/java/fr/silenthill99/madeinabyss/data/tags/BlockTagGenerator.java
@@ -0,0 +1,28 @@
+package fr.silenthill99.madeinabyss.data.tags;
+
+import fr.silenthill99.madeinabyss.Main;
+import fr.silenthill99.madeinabyss.init.ModBlocks;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
+import net.minecraftforge.common.data.BlockTagsProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class BlockTagGenerator extends BlockTagsProvider {
+    public BlockTagGenerator(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, lookupProvider, Main.MODID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider provider) {
+        tag(BlockTags.MINEABLE_WITH_PICKAXE).add(
+                ModBlocks.ABYSSIUM.get()
+        );
+        tag(BlockTags.NEEDS_IRON_TOOL).add(
+                ModBlocks.ABYSSIUM.get()
+        );
+    }
+}
